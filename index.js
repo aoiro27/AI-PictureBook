d133ad2ffa4f0870e036d092af18d8a34f3a724d
aoiro27 <aoiro.savant@gmail.com>
2025-07-14 23:48:57 +0900

    init

---
+++ index.js
@@ -0,0 +1,35 @@
+import { GoogleGenAI, Modality } from "@google/genai";
+import * as fs from "node:fs";
+
+async function main() {
+
+    console.log(process.env.GOOGLE_API_KEY);
+  const ai = new GoogleGenAI({
+    apiKey: process.env.GOOGLE_API_KEY
+  });
+
+  const contents =
+    "ある日、二人はお庭で大きなダンゴムシを見つけました。「わあ！まるまってる！」としろちゃんが言いました。「これはダンゴムシっていうんだよ。怖がって丸くなるんだ」としきちゃんが教えてくれました。";
+
+  // Set responseModalities to include "Image" so the model can generate  an image
+  const response = await ai.models.generateContent({
+    model: "gemini-2.0-flash-preview-image-generation",
+    contents: contents,
+    config: {
+      responseModalities: [Modality.TEXT, Modality.IMAGE],
+    },
+  });
+  for (const part of response.candidates[0].content.parts) {
+    // Based on the part type, either show the text or save the image
+    if (part.text) {
+      console.log(part.text);
+    } else if (part.inlineData) {
+      const imageData = part.inlineData.data;
+      const buffer = Buffer.from(imageData, "base64");
+      fs.writeFileSync("gemini-native-image.png", buffer);
+      console.log("Image saved as gemini-native-image.png");
+    }
+  }
+}
+
+main();
